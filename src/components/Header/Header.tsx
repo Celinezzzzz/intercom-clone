aa35ff6f77bbf024eefcecbe7d6fe3988c4374ba
Si Min Zhou <szhou389@gatech.edu>
2024-12-30 00:41:44 -0800

    add Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.scss'; 
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="nav__container-links">
+          <a className="nav__icon" href="#">
+            <img src="/src/assets/images/icon-1.jpg" alt="icon-1" />
+          </a>
+          <ul className="nav__list">
+            <li className="nav__item"><a href="#" className="nav__link">Product</a></li>
+            <li className="nav__item"><a href="#" className="nav__link">Resources</a></li>
+            <li className="nav__item"><a href="#" className="nav__link">Pricing</a></li>
+          </ul>
+        </div>
+        <div className="nav__container-actions">
+          <a className="nav__icon nav__icon--hidden" href="#">
+            <img src="./assets/images/icon-2.jpg" alt="icon-2" />
+          </a>
+          <ul className="nav__list">
+            <li className="nav__item"><a href="#" className="nav__link">Contact sales</a></li>
+            <li className="nav__item"><a href="#" className="nav__link">Sign in</a></li>
+            <li className="nav__item"><a href="#" className="nav__link">View demo</a></li>
+            <li className="nav__item"><a href="#" className="nav__link nav__link--button">Start free trial</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
